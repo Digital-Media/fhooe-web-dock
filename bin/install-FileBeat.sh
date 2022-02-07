451b642323234fc9b9867a28bb4381dbcea4d27c
martinharrer <martin.harrer@fh-hagenberg.at>
2022-02-07 15:31:10 +0100

    removed installation files, that are no longer necessary. Those necessary for webshop have been replaced bei docker-compose.yml and Dockerfiles. Those, that are neccessary for big data moved to that repository

--- bin/install-FileBeat.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install filebeat
-sudo bash -c "echo 'sudo systemctl start filebeat' >> /usr/local/bin/StartFilebeat.sh"
-sudo bash -c "echo 'sudo systemctl stop filebeat' >> /usr/local/bin/StopFilebeat.sh"
-sudo chmod a+x /usr/local/bin/*Filebeat.sh
-
