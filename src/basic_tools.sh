3c2afc2931f959a4c2f50d9d60791b2450dee4a2
martinharrer <martin.harrer@fh-hagenberg.at>
2022-12-21 12:45:14 +0100

    added psmisc

--- src/basic_tools.sh
+++ src/basic_tools.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "## Installing zip, unzip, curl, net-tools ##"
-apt-get -y install curl git iputils-ping less nano net-tools openssl telnet unzip vim wget zip
+apt-get -y install curl git iputils-ping less nano net-tools openssl psmisc telnet unzip vim wget zip
