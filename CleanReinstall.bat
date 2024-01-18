20f1bc56cb6f0af030eae57a27a5d3380913c76b
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2024-01-18 14:43:42 +0100

    Updated CleanReinstall.bat
    Security question before removing all containers and images. Harmonized with CleanReinstall.sh

--- CleanReinstall.bat
+++ CleanReinstall.bat
@@ -1,5 +1,24 @@
 @echo off
 
+echo Stopping all running fhooe-web-dock containers
+docker compose stop
+
+echo These containers will be deleted together with their volumes and images and afterwards recreated:
+docker ps -f "status=exited"
+
+:prompt
+set /p "answer=Continue? [Y/n] "
+if "%answer%"=="" set answer=Y
+
+if /i "%answer%"=="Y" goto proceed
+if /i "%answer%"=="y" goto proceed
+if /i "%answer%"=="N" goto cancel
+if /i "%answer%"=="n" goto cancel
+
+echo Please answer Y or n to continue.
+goto prompt
+
+:proceed
 echo Stop all running containers
 docker compose down --volumes
 
@@ -14,3 +33,10 @@ docker compose up --detach
 
 echo All finished. Enjoy your updated version of fhooe-web-dock!
 pause
+exit
+
+:cancel
+echo Containers, images and volumes are not removed. Keeping the current versions and restarting...
+docker compose start
+pause
+exit
